a61bfa0e9ae720c6118175dd60783a6afd523c2f
Chris Messer <chrismesser@Chriss-MBP.attlocal.net>
2023-01-15 21:05:46 -0600

    fixed buttons

--- buttons_dir.py
+++ buttons_dir.py
@@ -1,6 +1,7 @@
 from gpiozero import Button
 from services.print_budget import refresh_budget
 from utils.print_to_eink import print_text
+from utils.build_images import plain_text
 from PIL import Image
 from dotenv import load_dotenv
 import logging
@@ -16,18 +17,21 @@ btn3 = Button(13)
 btn4 = Button(19)
 monthly_budget = os.getenv('monthly_budget')
 
-def handleBtnPress(btn):
-    pinNum = btn.pin.number
-    switcher = {
-        5: refresh_budget(monthly_budget, _debug=False),
-        6: print_text(Image.open('images/piggy.bmp')),
-        13: "Hope you lik it.",
-        19: "Goodbye!"
-    }
-    msg = switcher.get(pinNum, "Error")
+def key1():
+    refresh_budget(monthly_budget, _debug=False)
 
-btn1.when_pressed = handleBtnPress
-btn2.when_pressed = handleBtnPress
-btn3.when_pressed = handleBtnPress
-btn4.when_pressed = handleBtnPress
+def key2():
+    print_text(Image.open('images/piggy.bmp'))
+
+def key3():
+    print_text(plain_text("Key 3 Pressed"))
+
+def key4():
+    print_text(plain_text("Key 4 Pressed"))
+
+
+btn1.when_pressed = key1
+btn2.when_pressed = key2
+btn3.when_pressed = key3
+btn4.when_pressed = key4
 
