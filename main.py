55ec65d895c981d5c783bd5b42cc1cb3fb6b6e16
Chris Messer <chrismesser@Chriss-MBP.attlocal.net>
2023-01-29 15:26:08 -0600

    fixed thread order to avoid stack overflow

--- main.py
+++ main.py
@@ -12,25 +12,28 @@ load_dotenv()
 monthly_budget = int(os.getenv('monthly_budget'))
 
 def refresh_budget_thread(monthly_budget, _debug=False):
-    refresh_budget(monthly_budget, _debug=False)
-    time.sleep(1800)
+    while True:
+        refresh_budget(monthly_budget, _debug=False)
+        time.sleep(1800)
 
 def refresh_screen_thread():
-    refresh_screen()
-    time.sleep(175)
-
-while True:
-
-    api_pull=threading.Thread(target=refresh_budget,
-                              args=(monthly_budget, False))
-    screen_refresh=threading.Thread(target=refresh_screen_thread)
-
-    button_listen=threading.Thread(target=buttons_dir.button_listen)
-
-    api_pull.start()
-    button_listen.start()
-    time.sleep(180)
-    screen_refresh.start()
+    while True:
+        time.sleep(175)
+        refresh_screen()
 
 
-    print('pause')
+
+
+api_pull=threading.Thread(target=refresh_budget,
+                          args=(monthly_budget, False))
+screen_refresh=threading.Thread(target=refresh_screen_thread)
+
+button_listen=threading.Thread(target=buttons_dir.button_listen)
+
+api_pull.start()
+button_listen.start()
+
+screen_refresh.start()
+
+
+print('pause')
